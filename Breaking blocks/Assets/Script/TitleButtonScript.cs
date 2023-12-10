7f4417f22467575471c480304a925b7a61c6c895
Takumi-666 <108921895+Takumi-666@users.noreply.github.com>
2023-12-10 22:20:41 +0900

    タイトルに遷移するボタンの追加

---
+++ Breaking blocks/Assets/Script/TitleButtonScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleButtonScript : MonoBehaviour
+{
+    public void OnTitleButtonPressed()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+}
