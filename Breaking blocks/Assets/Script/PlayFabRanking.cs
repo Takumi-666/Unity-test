a574e0cc9dc3a3ee602a8dd34f3b317ba376cd42
Takumi-666 <108921895+Takumi-666@users.noreply.github.com>
2023-12-10 21:16:52 +0900

    ランキング情報の表示

--- Breaking blocks/Assets/Script/PlayFabRanking.cs
+++ Breaking blocks/Assets/Script/PlayFabRanking.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using PlayFab;
 using PlayFab.ClientModels;
+using System.Text;
 
 public class PlayFabRanking : MonoBehaviour
 {
+    public Text Nameinput; // Nameinput用のText UI要素
+    public Text Scoreinput; // Scoreinput用のText UI要素
+
     void Start()
     {
         GetLeaderboard();
@@ -13,14 +18,24 @@ public class PlayFabRanking : MonoBehaviour
     {
         PlayFabClientAPI.GetLeaderboard(new GetLeaderboardRequest
         {
-            StatisticName = "testgame"
+            StatisticName = "testgame",
+            StartPosition = 0, // ランキングの開始位置
+            MaxResultsCount = 10 // 取得する最大結果数
         }, result =>
         {
+            StringBuilder names = new StringBuilder();
+            StringBuilder scores = new StringBuilder();
+
             foreach (var item in result.Leaderboard)
             {
                 string displayName = item.DisplayName ?? "NoName";
-                Debug.Log($"{item.Position + 1}位: {displayName} スコア {item.StatValue}");
+                names.AppendLine($"{item.Position + 1}位: {displayName}");
+                scores.AppendLine($"スコア {item.StatValue}");
             }
+
+            // Text UI要素に値を設定
+            Nameinput.text = names.ToString();
+            Scoreinput.text = scores.ToString();
         }, error =>
         {
             Debug.Log("ランキングデータ取得失敗: " + error.GenerateErrorReport());
